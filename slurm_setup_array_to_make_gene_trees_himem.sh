07046fe5000698d4ce0ba204fbf0af357b0fb7b0
Paul Bailey <P.Bailey@kew.org>
2021-07-16 13:03:31 +0100

    Added option -b which allows checkpointing for gene trees

--- slurm_setup_array_to_make_gene_trees_himem.sh
+++ slurm_setup_array_to_make_gene_trees_himem.sh
@@ -73,7 +73,7 @@ $maxColOccThreshold \
 "$trimAln1" \
 "$trimAln2" \
 "$treeshrink" \
-"$outgroupRoot"
+"$outgroupRoot" \
 "$checkpointing"
 ### 16.3.2020 - could remove this $$exePrefix - but it might be useful to see whether sub-processes get added to the mem used for this script.
 ### 3.6.2020 - yes they do - the time and mem from this command is the total of tiem within script AND max mem used within the child processes - good
